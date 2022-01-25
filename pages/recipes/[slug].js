b732cdc633fa62e14978ba48558762bb54c7412d
unknown <hello@dominiccho.co>
2022-01-25 12:56:25 +0800

    initial

---
+++ pages/recipes/[slug].js
@@ -0,0 +1,103 @@
+import Image from 'next/image'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { createClient } from 'contentful'
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+})
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({
+    content_type: 'recipe',
+  })
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    }
+  })
+
+  return {
+    paths: paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'recipe',
+    'fields.slug': params.slug,
+  })
+
+  return {
+    props: { recipe: items[0] },
+  }
+}
+
+export default function RecipeDetails({ recipe }) {
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields
+
+  console.log(recipe)
+  return (
+    <div>
+      <div className='banner'>
+        <Image
+          src={'https:' + featuredImage.fields.file.url}
+          width={1200}
+          height={600}
+        />
+        <h2>{title}</h2>
+      </div>
+
+      <div className='info'>
+        <p>Take about {cookingTime} mins to cook</p>
+        <h3>Ingredients:</h3>
+        {ingredients.map((ing) => (
+          <span key={ing}>{ing}</span>
+        ))}
+      </div>
+
+      <div className='method'>
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+      <style jsx>{`
+        .card {
+          transform: rotateZ(-1deg);
+        }
+        .content {
+          background: #fff;
+          box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+          margin: 0;
+          position: relative;
+          top: -40px;
+          left: -10px;
+        }
+        .info {
+          padding: 16px;
+        }
+        .info h4 {
+          margin: 4px 0;
+          text-transform: uppercase;
+        }
+        .info p {
+          margin: 0;
+          color: #777;
+        }
+        .actions {
+          margin-top: 20px;
+          display: flex;
+          justify-content: flex-end;
+        }
+        .actions a {
+          color: #fff;
+          background: #f01b29;
+          padding: 16px 24px;
+          text-decoration: none;
+        }
+      `}</style>
+    </div>
+  )
+}
